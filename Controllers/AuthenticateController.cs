fce6f0343945c520861bbe846efcf55ea094469a
steve <sgourley@utah.gov>
2013-07-03 19:05:12 -0600

    empty project

---
+++ Controllers/AuthenticateController.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Http;
+
+namespace ArcGisServerPermissionsProxy.Controllers
+{
+    public class AuthenticateController : ApiController
+    {
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        public void Post([FromBody] string value)
+        {
+        }
+    }
+}
