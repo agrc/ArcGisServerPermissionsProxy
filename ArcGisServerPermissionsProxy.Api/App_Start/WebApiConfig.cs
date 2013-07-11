31ad4a1efec959e83ad0d80c92af91f9f20f400d
steve <sgourley@utah.gov>
2013-07-11 14:34:28 -0600

    removed xml formatter

--- ArcGisServerPermissionsProxy.Api/App_Start/WebApiConfig.cs
+++ ArcGisServerPermissionsProxy.Api/App_Start/WebApiConfig.cs
@@ -1,4 +1,6 @@
 ﻿using System.Web.Http;
+using ArcGisServerPermissionsProxy.Api.Configuration.Ninject;
+using Newtonsoft.Json;
 
 namespace ArcGisServerPermissionsProxy.Api
 {
@@ -11,14 +13,14 @@ namespace ArcGisServerPermissionsProxy.Api
                 routeTemplate: "api/{controller}/{action}"
             );
 
-            // Uncomment the following line of code to enable query support for actions with an IQueryable or IQueryable<T> return type.
-            // To avoid processing unexpected or malicious queries, use the validation settings on QueryableAttribute to validate incoming queries.
-            // For more information, visit http://go.microsoft.com/fwlink/?LinkId=279712.
-            //config.EnableQuerySupport();
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
+            config.Formatters.JsonFormatter.SerializerSettings.NullValueHandling = NullValueHandling.Ignore;
 
-            // To disable tracing in your application, please comment out or remove the following line of code
-            // For more information, refer to: http://www.asp.net/web-api
+            GlobalConfiguration.Configuration.DependencyResolver = new NinjectDependencyResolver(App.Kernel);
+
+#if DEBUG
             config.EnableSystemDiagnosticsTracing();
+#endif
         }
     }
 }
