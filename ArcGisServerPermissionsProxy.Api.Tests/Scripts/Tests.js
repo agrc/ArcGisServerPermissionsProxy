8b8cce427e623e40e1c46c46c1ec711f266cea94
steve <sgourley@utah.gov>
2013-07-11 14:34:20 -0600

    added dojo and jasmine and js tests which won't work

---
+++ ArcGisServerPermissionsProxy.Api.Tests/Scripts/Tests.js
@@ -0,0 +1,43 @@
+﻿/// <reference path="jasmine.js" />
+/// <reference path="dojo/dojo.js" />
+
+require(["dojo/request"], function (request) {
+    describe("CanSendEmailWithPost", function () {
+        it("can post parameters", function () {
+            var data, parameters = JSON.stringify(
+                    {
+                        application:"test",
+                        adminEmails:["sgourley@utah.gov"]
+                    });
+
+            runs(function () {
+                request.post("http://arcgissecurity/api/admin/createapplication",
+                     {
+                         //CORS does not allow this to be set so the test fails
+                         headers: {
+                             "Content-Type": "application/json"
+                         },
+                         data: parameters,
+                         handleAs: "json"
+                     }).then(function (dataIn) {
+                         data = dataIn;
+                         console.log('success');
+                     }, function (err) {
+                         data = err;
+                         console.log('error function');
+                         console.dir(err);
+                     });
+            });
+
+            waitsFor(function () {
+                return data;
+            });
+
+            runs(function () {
+                console.dir(data);
+                expect(data).toBeDefined();
+                expect(data.status).toEqual(201);
+            });
+        });
+    });
+});
